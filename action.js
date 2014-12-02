8dd8cd69284017fba5d91ae54470d2e2b18d9807
CallMeSenpai <ericliao10797@hotmail.com>
2014-12-02 15:51:58 -0500

    almost home but on train ride

--- action.js
+++ action.js
@@ -1,29 +1,27 @@
 console.log("???");
-
 var random_num = function(x){
     return parseInt(Math.random()*x);
 }
 
-
 var obj=document.getElementById("cow");
 obj.addEventListener('click',
-			 function(e){
-			     console.log(e);
-			     console.log(this);
-			 });
-var button=document.getElementById("b");
-button.addEventListener('click',start);
+		     function(e){
+			 //console.log(e);
+			 //console.log(this);
+			 console.log("swiggity swag clicked circle");
+		     });
+//also add keyboard listener for osu play style
+
 
 var start=function(){
+    console.log("started your stupid game");
     setInterval(timer,500);
     function timer(){
-	console.log("printed line");
-	//read up on how to display cow based on form in html (hidden, etc)
-    
-	//also add keyboard listener for osu play style
+	//console.log("printed line");
+	//try hiding the cow
     }
 }
-
+var button=document.getElementById("b");
 button.addEventListener('click',start);
 
 //bg music???
