74ce97e044bd4b7206f13e5dbd91c1b6361a16d0
Aaron Mortenson <aamortenson1@gmail.com>
2014-12-04 10:15:45 -0500

    swag

--- action.js
+++ action.js
@@ -5,31 +5,33 @@ var toInt = function(e){
 var random_num = function(x){
     return parseInt(Math.random()*x);
 }
-function toRad(value) {
+var toRad = function(value) {
     /** Converts numeric degrees to radians */
     return (value * Math.PI) / 180;
 }
-function abs_value_from(value1,value2){
+var abs_value_from = function(value1,value2){
     /** Finds the difference between the 2 values **/
     /** used in heading changing **/
     return Math.abs(value1-value2);
 }
 
+var hide = function(){
+    console.log("hidden");
+    cow.src="invisible.png";
+}
+
 var width = document.documentElement.clientWidth;
-var height = document.documentElement.clientHeight;
-    
+var height = document.documentElement.clientHeight; 
 
 var cow=document.getElementById("cow");
-cow.speed=8;
+cow.style.cursor="pointer";
+cow.speed=0;
 cow.heading=0;
 cow.style.position="absolute";
 cow.style.left="200px";
 cow.style.top="200px";
-cow.addEventListener('click',
-		     function(e){
-			 console.log("swigity swag clicked circle");
-			 document.getElementById("trap").play();
-		     });
+hide();
+
 var changeLoc = function(x,y){
     cow.style.left=x;
     cow.style.top=y;
@@ -61,7 +63,7 @@ function timer(e){
     while(cow.heading<0){//mod doesnt deal with negatives?
 	cow.heading+=360;
     }
-    console.log(cow.heading);
+    //console.log(cow.heading);
     //console.log(width);	
 }
 var updateMusic = function(e){
@@ -75,13 +77,14 @@ var updateMusic = function(e){
     var deltaY = (y-mouseY)/(height-64);	
     var dist = Math.sqrt((deltaX*deltaX)+(deltaY*deltaY));
     var newVol = 1-dist;
-    console.log(newVol);
+    //console.log(newVol);
     music.volume = newVol;
 }
 
 var button=document.getElementById("b");
 var start=function(){
     console.log("started your stupid game");
+    cow.speed = 0;
     music = new Audio("cantina.mp3");
     music.addEventListener("ended",function () {
 	this.currentTime = 0;
@@ -89,6 +92,14 @@ var start=function(){
     }, false);
     music.play();
     window.addEventListener("mousemove",updateMusic);
-    
+    cow.addEventListener('click',
+		     function(e){
+			 console.log("swigity swag clicked circle");
+			 document.getElementById("trap").play();
+			 cow.speed+=1;
+			 console.log(cow.speed);
+			 cow.src="admiral.png";
+			 setTimeout(function(){hide()}, 1500);
+		     });
 }
 button.addEventListener('click',start);
